6dd09df7b56c07ff87278b3ca6b4d7d72c2f87fe
AlexStep <alex.step.kellas@gmail.com>
2017-06-13 14:25:58 +0200

    Fetch history from eth

--- src/model/txhistory.js
+++ src/model/txhistory.js
@@ -1,26 +1,66 @@
-import DB from 'DB/DB'
+import _config  from 'app.config'
+import DB  from 'DB/DB'
+import Eth from 'Eth/Eth'
 
-const DBtxhistory = DB.data.get('txhistory')
+const DBtxhistory = DB.data.get('txhistory7')
 
 let _history = {}
 
 export default new class TXHistory {
 	constructor() {
-		DBtxhistory.map().on( (item, time) => {
-			_history[time] = item
+		DBtxhistory.map().on( (item, hash) => {
+			_history[hash] = item
 		} )
+
+		this.load()
+		setInterval(()=>{
+			this.load()
+		}, 60*1000)
+	}
+
+	load(callback){
+		let tx_url = 'http://'+_config.network+'.etherscan.io/api?module=account&action=txlist&address=' + Eth.Wallet.get().openkey +'&startblock=0&endblock=latest'
+
+		fetch(tx_url).then( r => {
+			return r.json()
+		}).then( r=>{
+			if (!r.result) { return }
+			let txs = r.result.reverse()
+
+
+			for(let k in txs){
+				let tx = txs[k]
+
+				if (!(tx.value*1)) { continue }
+
+				if (!this.find(tx.hash)) {
+					this.add({
+						time:   tx.timeStamp*1000,
+						out:    !(tx.to==Eth.Wallet.get().openkey),
+						tokens: false,
+						tx:     tx.hash,
+						to:     tx.to,
+						amount: tx.value/1000000000000000000,
+					})
+				}
+			}
+		})
+
 	}
 
 	add(data){
-		data.time = new Date().getTime()
-		_history[data.time] = data
-		DBtxhistory.get(data.time).put( _history[data.time] )
+		if (!data.time ) {
+			data.time = new Date().getTime()
+		}
+
+		_history[data.tx] = data
+		DBtxhistory.get(data.tx).put( _history[data.tx] )
 	}
 
 	get(){
 		let history = []
 		for(let k in _history){
-			_history[k].time = new Date(k*1).toString()
+			_history[k].time = new Date( _history[k].time ).toString()
 			history.push( _history[k] )
 		}
 
@@ -29,6 +69,10 @@ export default new class TXHistory {
 			if(a.time < b.time) return 1
 			return 0
 		})
+	}
+
+	find(hash){
+		return _history[hash]
 	}
 
 	subscribe(callback){
--- src/view/components/screens/wallet/history.tag
+++ src/view/components/screens/wallet/history.tag
@@ -53,11 +53,11 @@ import './history.less'
 
 			<label if={trans.from}><span>from:</span>
 				<input onclick={copy} value={trans.from} class="from" type="text">
-				<a class="blockchain" href="{etherscan_url}/taddress/{trans.tx}" target="_blank" rel="noopener">blockchain</a>
+				<a class="blockchain" href="{etherscan_url}/address/{trans.from}" target="_blank" rel="noopener">blockchain</a>
 			</label>
 			<label if={trans.to}><span>to:</span>
 				<input onclick={copy} value={trans.to} class="tx" type="text">
-				<a class="blockchain" href="{etherscan_url}/address/{trans.tx}" target="_blank" rel="noopener">blockchain</a>
+				<a class="blockchain" href="{etherscan_url}/address/{trans.to}" target="_blank" rel="noopener">blockchain</a>
 			</label>
 		</div>
 	</div>
