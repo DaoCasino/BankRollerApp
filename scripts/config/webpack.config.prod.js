d9c3c55c94334828153cffd68a817fdc204f2e5e
AlexStep <alex.step.kellas@gmail.com>
2017-07-26 13:44:20 +0200

    add babel polyfill to prod config

--- scripts/config/webpack.config.prod.js
+++ scripts/config/webpack.config.prod.js
@@ -75,7 +75,7 @@ let webpack_prod_config = {
 	devtool: 'source-map',
 
 	// In production, we only want to load the polyfills and the app code.
-	entry: [require.resolve('./polyfills'), paths.appIndexJs],
+	entry: ['babel-polyfill', require.resolve('./polyfills'), paths.appIndexJs],
 	output: {
 		// The build folder.
 		path: paths.appBuild,
