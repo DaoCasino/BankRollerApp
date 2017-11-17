bc856b46b97154fc5199c1bd3be5970e1ae56647
Alex Step <alex.step.kellas@gmail.com>
2017-11-17 13:17:37 +0200

    fixes

--- src/model/Eth/Wallet.js
+++ src/model/Eth/Wallet.js
@@ -261,9 +261,8 @@ export default class Wallet {
 
 		hash = Utils.add0x(hash)
 		if (this.web3.utils.isHexStrict(hash)) {
-			console.log(hash+' is not correct hex')
+			console.log(hash+' is incorrect hex')
 			console.log('Use DCLib.Utils.makeSeed or Utils.soliditySHA3(your_args) to create valid hash')
-			throw new Error('invalid hash')
 		}
 
 		return signHash(hash, Utils.add0x(_privkey))
