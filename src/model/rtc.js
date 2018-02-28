89119766fd6b3ffc318c6eab7606ee4f7db58376
Alex Step <alex.step.kellas@gmail.com>
2018-02-28 22:19:59 +0200

    update rtc signals

--- src/model/rtc.js
+++ src/model/rtc.js
@@ -68,8 +68,9 @@ const ipfs = new IPFS({
 	config: {
 		Addresses: {
 			Swarm: [
-				'/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
-				// '/ip4/127.0.0.1/tcp/4001'
+				'/ip4/46.101.244.101/tcp/9090/ws/p2p-websocket-star/',
+				'/ip4/146.185.173.84/tcp/9090/ws/p2p-websocket-star/',
+				// '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
 			]
 		}
 	}
